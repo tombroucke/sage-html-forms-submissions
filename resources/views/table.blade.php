d4d032485f5f7ea913ee9cdcb0c1ce9eb13d258d
Tom Broucke <tom@tombroucke.be>
2025-03-31 12:09:54 +0200

    ✨ feat: hide table with 0 entries

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,20 +1,22 @@
-<div class="sage-html-forms-submissions">
-  <table class="table">
-    <thead>
-      <tr>
-        @foreach ($fields as $key => $label)
-          <th>{{ esc_html($label) }}</th>
-        @endforeach
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($submissions as $submission)
+@unless (empty($submissions))
+  <div class="sage-html-forms-submissions">
+    <table class="table">
+      <thead>
         <tr>
           @foreach ($fields as $key => $label)
-            <td>{{ esc_html($submission[$key] ?? '-') }}</td>
+            <th>{{ esc_html($label) }}</th>
           @endforeach
         </tr>
-      @endforeach
-    </tbody>
-  </table>
-</div>
+      </thead>
+      <tbody>
+        @foreach ($submissions as $submission)
+          <tr>
+            @foreach ($fields as $key => $label)
+              <td>{{ esc_html($submission[$key] ?? '-') }}</td>
+            @endforeach
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+@endunless
