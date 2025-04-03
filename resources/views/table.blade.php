d4f883dd351446527d98337f9ab305d6fa7f62fe
Tom Broucke <tom@tombroucke.be>
2025-04-03 09:35:38 +0200

    🩹 fix: Make table responsive

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,22 +1,24 @@
 @unless (empty($submissions))
   <div class="sage-html-forms-submissions">
-    <table class="table">
-      <thead>
-        <tr>
-          @foreach ($fields as $key => $label)
-            <th>{{ Str::replace('&#039;', "'", $label) }}</th>
-          @endforeach
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($submissions as $submission)
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
           <tr>
             @foreach ($fields as $key => $label)
-              <td>{{ Str::replace('f#039;', "'", $submission[$key] ?? '-') }}</td>
+              <th>{{ Str::replace('&#039;', "'", $label) }}</th>
             @endforeach
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @foreach ($submissions as $submission)
+            <tr>
+              @foreach ($fields as $key => $label)
+                <td>{{ Str::replace('f#039;', "'", $submission[$key] ?? '-') }}</td>
+              @endforeach
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 @endunless
